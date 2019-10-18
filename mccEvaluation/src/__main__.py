76509de9a16c2dc3b67f764810fd79d3017c335f
marisaKoe <marisa.koellner@student.uni-tuebingen.de>
2019-10-18 11:32:16 +0200

    added changes for mrbayes

--- mccEvaluation/src/__main__.py
+++ mccEvaluation/src/__main__.py
@@ -9,15 +9,44 @@ evaluate the mcc scores to find the most optimal method
 import glob
 from collections import defaultdict
 
-def read_files(method):
+def read_files_DB():
     '''
-    read all files from a folder for a specific method.
-    :param method: the name of the method block 
+    read all files from a folder for the distance based method
     :return mcc_dict: dict with key=concept value=dict with key=method value=mcc_score
     :return eval_dict: dict with key=method value=o (int)
     '''
     ##get all the files from the folder
-    files = glob.glob("input/"+method+"/*.csv")
+    files = glob.glob("input/DB/*.csv")
+    ##initialize dict with key = concept value = dict with key=method value=mcc_score
+    mcc_dict = defaultdict(lambda: defaultdict(float))
+    eval_dict = defaultdict(int)
+    for fileName in files:
+        
+        f = open(fileName)
+        raw_data = f.readlines()
+        f.close()
+        method1 = fileName.split(".")[0].split("/")[-1].split("+")[-1]
+        print method1
+        eval_dict[method1]=0
+        
+        for l in raw_data:
+            l = l.split("\t")
+            concept = l[0].split("+")[0].split("_")[-1]
+
+            mcc_score=float(l[1])
+            mcc_dict[concept][method1]=mcc_score
+    
+
+    return mcc_dict, eval_dict
+
+def read_files_ML():
+    '''
+    read all files from a folder for the maximum likelihood method.
+    :return mcc_dict: dict with key=concept value=dict with key=method value=mcc_score
+    :return eval_dict: dict with key=method value=o (int)
+    '''
+    ##get all the files from the folder
+    files = glob.glob("input/ML/*.csv")
     ##initialize dict with key = concept value = dict with key=method value=mcc_score
     mcc_dict = defaultdict(lambda: defaultdict(float))
     eval_dict = defaultdict(int)
@@ -27,18 +56,66 @@ def read_files(method):
         raw_data = f.readlines()
         f.close()
         method = fileName.split(".")[0].split("/")[-1].split("+")[-1]
+        #print method
         eval_dict[method]=0
         
         for l in raw_data:
             l = l.split("\t")
-            concept = l[0].split("+")[0].split("_")[-1]
-            mcc_score=float(l[1])
-            mcc_dict[concept][method]=mcc_score
+            if method == "Tcoffee":
+                concept = l[0].split("_")[0]
+                mcc_score=float(l[1])
+                
+                mcc_dict[concept][method]=mcc_score
+                #print concept
+            else:
+                concept = l[0].split("_")[-1]
+                #print concept
+
+                mcc_score=float(l[1])
+                mcc_dict[concept][method]=mcc_score
     
 
     return mcc_dict, eval_dict
 
-def find_optimal_method(method):
+def read_files_MB():
+    '''
+    read all files from a folder for the maximum likelihood method.
+    :return mcc_dict: dict with key=concept value=dict with key=method value=mcc_score
+    :return eval_dict: dict with key=method value=o (int)
+    '''
+    ##get all the files from the folder
+    files = glob.glob("input/MB/*.csv")
+    ##initialize dict with key = concept value = dict with key=method value=mcc_score
+    mcc_dict = defaultdict(lambda: defaultdict(float))
+    eval_dict = defaultdict(int)
+    for fileName in files:
+        
+        f = open(fileName)
+        raw_data = f.readlines()
+        f.close()
+        method = fileName.split(".")[0].split("/")[-1].split("+")[-1]
+        print method
+        eval_dict[method]=0
+        
+        for l in raw_data:
+            l = l.split("\t")
+            if method == "tcoffee" or method=="PMICog":
+                concept = l[0].split("_")[0]
+                mcc_score=float(l[1])
+                
+                mcc_dict[concept][method]=mcc_score
+                #print concept
+            else:
+                concept = l[0].split("_")[-1]
+                #print concept
+
+                mcc_score=float(l[1])
+                mcc_dict[concept][method]=mcc_score
+    
+
+    return mcc_dict, eval_dict
+
+def find_optimal_method(mcc_dict, eval_dict, method):
     '''
     for each concept, get the highest mcc score and the method
     count the number of best concepts for each method
@@ -46,9 +123,12 @@ def find_optimal_method(method):
     :param metohd: name of the method block
     :return dict: eval
     '''
-    mcc_dict, eval_dict = read_files(method)
-
+    
+    #elif method == "MB":
+    #    mcc_dict, eval_dict = read_files_MB(method)
+    print len(mcc_dict)
     for concept, score_dict in mcc_dict.items():
+        #print len(score_dict)
         method_max_score = max(score_dict, key=lambda k: score_dict[k])
         eval_dict[method_max_score]+=1
     
@@ -70,9 +150,18 @@ if __name__ == '__main__':
     ##methods names: DBcog, DBwords, CBml, CBmb
     #read_files("mccPMI")
     #list_methods=["DBcog","DBwords"]
-    list_methods=["ML","DB"]
+    list_methods=["MB"]
     for method in list_methods:
-        find_optimal_method(method)
+        if method == "DB":
+            mcc_dict, eval_dict = read_files_DB()
+            find_optimal_method(mcc_dict, eval_dict, method)
+        elif method =="ML":
+            mcc_dict, eval_dict = read_files_ML()
+            find_optimal_method(mcc_dict, eval_dict, method)
+        elif method == "MB":
+            mcc_dict, eval_dict = read_files_MB()
+            find_optimal_method(mcc_dict, eval_dict, method)
+        
         
     
     
